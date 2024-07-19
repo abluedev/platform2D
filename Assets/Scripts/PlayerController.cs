c87f516c41ac129ae3dc0b0f4898cb044be2e978
abluedev <alexbluedev@gmail.com>
2024-07-19 16:19:38 +0200

    first commit!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    
     private readonly float SPEED = 5f;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,14 +22,19 @@ public class PlayerController : MonoBehaviour
         if (direction == 1)
         {
             _rigidbody2D.velocity = new Vector2(SPEED, _rigidbody2D.velocity.y);
+            _animator.SetBool("isRunning", true);
+
         }
         else if (direction == -1)
         {
-            _rigidbody2D.velocity = new Vector2(- SPEED, _rigidbody2D.velocity.y);
+            _rigidbody2D.velocity = new Vector2(-SPEED, _rigidbody2D.velocity.y);
+            _animator.SetBool("isRunning", true);
         }
         else
         {
             _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+            _animator.SetBool("isRunning", false);
+
         }
     }
 }
